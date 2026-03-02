1f1908d2ce1849775d5653f10787c2408ccf2abf
Fire3Duck <pauavellanedalopez@gmail.com>
2026-03-02 09:30:24 +0100

    Abanzes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent _enemyAgent;
+
+
+    public enum EnemyState
+    {
+        Patrolling,
+        Chasing,
+        Attacking
+    }
+
+
+    public EnemyState currentState;
+    Transform _player;
+    [SerializeField] private Transform[] _patrolPoints;
+    public int indexPatrolling;
+
+
+    [SerializeField] private float _detectionRange = 4;
+    [SerializeField] private float _attackRange = 2;
+   
+    void Awake()
+    {
+        _enemyAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindWithTag("Player").transform;
+    }
+
+
+    void Start()
+    {
+        currentState = EnemyState.Patrolling;
+        SetRandomPatrolPoint();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+            break;
+            case EnemyState.Chasing:
+                Chase();
+            break;
+            case EnemyState.Attacking:
+                Attack();
+            break;
+            default:
+                Patrol();
+            break;
+        }
+    }
+
+
+    void Patrol()
+    {
+        if(OnRange(_detectionRange))
+        {
+            currentState = EnemyState.Chasing;
+        }
+
+
+        if(_enemyAgent.remainingDistance < 0.5f)
+        {
+            SetRandomPatrolPoint();
+        }
+    }
+
+
+    void Chase()
+    {
+        if(!OnRange(_detectionRange))
+        {
+            currentState = EnemyState.Patrolling;
+        }
+
+
+        if(OnRange(_attackRange))
+        {
+            currentState = EnemyState.Attacking;
+            attackTimer = attackDelay;
+        }
+        _enemyAgent.SetDestination(_player.position);
+    }
+
+
+    float attackTimer;
+
+
+    float attackDelay = 2;
+
+
+    void Attack()
+    {
+        if(OnRange(_attackRange))
+        {
+            currentState = EnemyState.Chasing;
+        }
+
+
+        if(attackTimer < attackDelay)
+        {
+            attackTimer+= Time.deltaTime;
+
+
+            return;
+        }
+
+
+        Debug.Log("Attack");
+
+
+        attackTimer = 0;
+    }
+
+
+    void SetRandomPatrolPoint()
+    {
+        _enemyAgent.SetDestination(_patrolPoints[Random.Range(0, _patrolPoints.Length)].position);
+        indexPatrolling = (indexPatrolling + 1) % _patrolPoints.Length;
+    }
+
+
+    bool OnRange(float distance)
+    {
+        if(Vector3.Distance(transform.position, _player.position) < distance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
+        Gizmos.DrawWireSphere(transform.position, 10);
+    }
+
+}
